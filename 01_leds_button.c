3399ed5799217a313a5bc511340b71fa549e5d58
lklr89 <45421053+lklr89@users.noreply.github.com>
2019-02-13 21:55:31 +0100

    Update 01_leds_button.c

--- 01_leds_button.c
+++ 01_leds_button.c
@@ -1,7 +1,5 @@
 /*
-  leds_button.c
-
-  Created on: 20.04.2017
+  Control Launchpad LEDs with button S2
        Author: lklr
 */
 #include"msp430g2553.h"
@@ -11,21 +9,25 @@ int main(void) {
 
   // stop watchdog timer
   WDTCTL = WDTPW | WDTHOLD;
-  // P1 as digital IO
+  
+  // use P1 as digital IO
   P1SEL = 0x00;
-  // set up bit 0 of P1 as output
+  
+  // activate BIT0 and BIT6 as output 
   P1DIR |= 0x41; // =BIT6 (0x40) AND BIT0 (0x01)
-  // set BIT3 (Button S2) 0 -> input
+  
+  // set BIT3 (button S2) 0 -> input
   P1DIR &= ~BIT3;
-  // activate Pull-up 
+  
+  // activate Pull-up for button 
   P1REN |= BIT3;
+  
   // set BIT0 and BIT6 to 0 
   P1OUT &= ~(0x41);
-  // initialize BIT3 to 1 
+  
+  // initialize BIT3 (button S2) to 1 
   P1OUT |= BIT3;
 
-
-  // loop forever
   while(1)
   {
     if (!(P1IN & BIT3))
@@ -34,6 +36,7 @@ int main(void) {
       P1OUT ^= 0x40;
       // delay for a while
       for (i = 0; i < 32000; i++);
+      // toggle bit 6 of P1
       P1OUT ^= 0x01;
       while(!(P1IN & BIT3));
     }
