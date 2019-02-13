122b507e2328d292b6cb16ff91d8ddcdd1a15fee
lklr89 <45421053+lklr89@users.noreply.github.com>
2019-02-13 22:10:32 +0100

    Update 02_interrupts.c

--- 02_interrupts.c
+++ 02_interrupts.c
@@ -1,9 +1,13 @@
+/*
+  Using 1 timer interrupt and 1 port (button) interrupt to control LEDs
+  Author: lklr
+*/
+
 #include"msp430g2553.h"
 
-volatile int count = 0;
-
-int main() {
-
+int main() 
+{
+  // Setup buttons and LEDs..  
   WDTCTL = WDTPW | WDTHOLD;
   P1SEL = 0x00;
   P1DIR &= 0x00;
@@ -18,34 +22,36 @@ int main() {
   /*---------------
     TIMER INTERRUPT
   ----------------*/
-  CCTL0 = CCIE; // CCTL0 = TACCTL0 = Timer A capture/compare control 0
+  CCTL0 = CCIE; // CCTL0 = TACCTL0 = Timer A capture/compare control register 0 
                 // CCIE = capture/compare interrupt enable
   TACTL = TASSEL_2 + MC_1 + ID_3; // TACTL = Timer A Control Register
                                   // TASSEL_2 Timer A clock source select -> SMCLK (Subsystem Master Clock) ~1,1MHz
-                                  // MC_1 = Timer A Mode Control = aufwärts zählen
+                                  // MC_1 = Timer A Mode Control = count up
                                   // ID_3 = Timer A Input Divider = 8
-  CCR0 = 65535; // Timer A capture/compare Zählwert
-                // Interrupt Frequenz = 1,1 MHz / 8 / 65535 = 2,098 Hz
+  CCR0 = 65535; // Timer A capture/compare counting state
+                // Interrupt frequency = 1,1 MHz / 8 / 65535 = 2,098 Hz
 
   /*----------------
     PORT INTERRUPT
   -----------------*/
-  P1IE |= BIT3; // Interrupt Enable: Interrupt auf P1.3 ermöglichen
-  P1IES |= BIT3; // Interrupt Edge Select: Interrupt auf HiLo Signal-Flanke bei P1.3 ansetzen
-  P1IFG &= ~BIT3; // Interrupt flag für P1.3 auf 0 initialisieren
+  P1IE |= BIT3; // Interrupt Enable on P1.3 
+  P1IES |= BIT3; // Interrupt Edge Select: Interrupt on HiLo edge on P1.3 
+  P1IFG &= ~BIT3; // initialize Interrupt flag for P1.3 (to 0)
 
-  // Global Interrupts aktivieren
+  // activate Global Interrupts
   _EINT();
 
-  // Low Power Modus 0 aktivieren -
-  _BIS_SR(CPUOFF + GIE); // CPU deaktivieren, Global Interrupt Enable
+  // activate Low Power Mode 0 
+  _BIS_SR(CPUOFF + GIE); // deactiviate CPU , Global Interrupt Enable
   while(1);
 }
+
+volatile int count = 0;
 
 /*----------------
     TIMER ISR
 ----------------*/
-#pragma vector = TIMER0_A0_VECTOR
+#pragma vector = TIMER0_A0_VECTOR // Start-address is µc-specific! (interrupt vector table)
 __interrupt void ISR_TIMERA(void)
 {
   count++;
@@ -59,8 +65,8 @@ __interrupt void ISR_TIMERA(void)
 /*----------------
      PORT ISR
 -----------------*/
-#pragma vector = PORT1_VECTOR // Startadresse "0xFFE4 Port 1" der ISR festgelegt, µc-spezifisch!
-__interrupt void ISR_PORT1(void) // Interrupt Funktion
+#pragma vector = PORT1_VECTOR // Start-address is µc-specific! (interrupt vector table)
+__interrupt void ISR_PORT1(void) 
 {
   volatile int i;
   P1OUT ^= 0x40;
